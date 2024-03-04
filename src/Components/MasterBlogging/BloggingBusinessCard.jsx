8a90b08cd648e501ce1a6a9b1acaa2ce83c056b5
Hammad Rafiq <hammadrafiq1@gmail.com>
2024-03-04 21:40:16 +0500

    img bug fixes

--- src/Components/MasterBlogging/BloggingBusinessCard.jsx
+++ src/Components/MasterBlogging/BloggingBusinessCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
 import Typography from '@mui/material/Typography';
-import BusinessIcon from '../../Assets/business_icon.svg'
+import DefaultImg from '../../Assets/business_icon.svg'
 
-const BloggingBusinessCard = ({item}) => {
+const BloggingBusinessCard = ({ item }) => {
     return (
         <>
             <Grid item xs={12} md={6} lg={4}>
@@ -15,7 +15,7 @@ const BloggingBusinessCard = ({item}) => {
                     padding: '24px 32px 24px'
                 }}>
                     <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
-                        <img src={item.icon.src} alt="Business Icon" />
+                        <img className='card-icon-primary' src={item?.icon?.src ?? DefaultImg} alt="Business Icon" />
                         <Typography variant="h2" sx={{
                             fontSize: '20px',
                             fontWeight: 500,
@@ -37,19 +37,19 @@ const BloggingBusinessCard = ({item}) => {
                         <Typography sx={{
                             color: 'var(--purple)',
                             fontWeight: 500,
-                            border:'1px solid var(--purple)',
+                            border: '1px solid var(--purple)',
                             fontSize: '14px',
                             lineHeight: '22px',
                             borderRadius: '48px',
                             background: 'rgba(107, 99, 251, 0.15)',
                             display: 'inline-block',
                             padding: '8px 24px',
-                            marginRight:'6px'
+                            marginRight: '6px'
                         }} variant="body2">Business</Typography>
                         <Typography sx={{
                             color: 'var(--purple)',
                             fontWeight: 500,
-                            border:'1px solid var(--purple)',
+                            border: '1px solid var(--purple)',
                             fontSize: '14px',
                             lineHeight: '22px',
                             borderRadius: '48px',
--- src/Components/MasterBlogging/BloggingJobCard.jsx
+++ src/Components/MasterBlogging/BloggingJobCard.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
 import Typography from '@mui/material/Typography';
-import AnimationIcon from '../../Assets/courses/animation.svg'
+import DefaultImg from "../../Assets/blogging_job.svg"
 
-const BloggingJobCard = ({item}) => {
+const BloggingJobCard = ({ item }) => {
   return (
     <>
-    <Grid item xs={12} md={6} lg={4}>
+      <Grid item xs={12} md={6} lg={4}>
         <Box className="blogging-job-card" sx={{
-                borderRadius:'16px',
-                border:'1px solid var(--purple)',
-                background:'rgba(107, 99, 251, 0.10)',
-                padding:'24px 32px 32px'
-                }}>
-                <Box sx={{display:'flex',gap:'15px'}}>
-                    <img src={item.icon?.src} width="50px" height="auto" alt="Animation Icon"/>
-                    <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
-                        <h2 class="card-heading card-heading-inner">{item.title}</h2>
-                        <Typography sx={{fontSize:'18px',marginTop:'5px'}} variant="body2">
-                       {item.owner}
-                        </Typography>
-                    </Box>
+          borderRadius: '16px',
+          border: '1px solid var(--purple)',
+          background: 'rgba(107, 99, 251, 0.10)',
+          padding: '24px 32px 32px'
+        }}>
+          <Box sx={{ display: 'flex', gap: '15px' }}>
+            <img className='card-icon-primary' src={item.icon?.src ?? DefaultImg} alt="Animation Icon" />
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+              <h2 class="card-heading card-heading-inner">{item.title}</h2>
+              <Typography sx={{ fontSize: '18px', marginTop: '5px' }} variant="body2">
+                {item.owner}
+              </Typography>
+            </Box>
 
-                </Box>
-                <Box sx={{marginTop:'70px'}}>
-                    <p class="card-description">{item.desc}</p>
-                </Box>
+          </Box>
+          <Box sx={{ marginTop: '70px' }}>
+            <p class="card-description">{item.desc}</p>
+          </Box>
         </Box>
-    </Grid>
+      </Grid>
     </>
   )
 }
